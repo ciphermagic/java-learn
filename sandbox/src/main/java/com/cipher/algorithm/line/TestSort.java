166d654c3843c31a78e88864da63667568198df4
ciphermagic <ciphermagic@yeah.net>
2018-09-18 15:49:21 +0800

    归并排序

--- sandbox/src/main/java/com/cipher/algorithm/line/TestSort.java
+++ sandbox/src/main/java/com/cipher/algorithm/line/TestSort.java
@@ -1,5 +1,7 @@
 package com.cipher.algorithm.line;
 
+import java.util.Arrays;
+
 import static com.cipher.algorithm.line.SortTestHelper.*;
 
 /**
@@ -8,9 +10,37 @@ import static com.cipher.algorithm.line.SortTestHelper.*;
 public class TestSort {
 
     public static void sort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j > 0 && nums[j] < nums[j - 1]; j--) {
-                swap(nums, j, j - 1);
+        _sort(nums, 0, nums.length - 1);
+    }
+
+    private static void _sort(int[] nums, int l, int r) {
+        System.out.println("Deal with [ " + l + " , " + r + " ]");
+        if (l >= r) {
+            return;
+        }
+        int mid = (l + r) / 2;
+        _sort(nums, l, mid);
+        _sort(nums, mid + 1, r);
+        merge(nums, l, mid, r);
+    }
+
+    private static void merge(int[] nums, int l, int mid, int r) {
+        int[] aux = Arrays.copyOfRange(nums, l, r + 1);
+        int i = l;
+        int j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                nums[k] = aux[j - l];
+                j++;
+            } else if (j > r) {
+                nums[k] = aux[i - l];
+                i++;
+            } else if (aux[i - l] < aux[j - l]) {
+                nums[k] = aux[i - l];
+                i++;
+            } else {
+                nums[k] = aux[j - l];
+                j++;
             }
         }
     }
