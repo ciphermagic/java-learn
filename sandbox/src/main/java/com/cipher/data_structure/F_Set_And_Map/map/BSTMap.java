e7659641f753a8d4bbbfae81c57cb4578aaa6682
ciphermagic <ciphermagic@yeah.net>
2018-11-08 11:28:40 +0800

    BSTMap

---
+++ sandbox/src/main/java/com/cipher/data_structure/F_Set_And_Map/map/BSTMap.java
@@ -0,0 +1,45 @@
+package com.cipher.data_structure.F_Set_And_Map.map;
+
+/**
+ * @Author: CipherCui
+ * @Description:
+ * @Date: Created in 11:27 2018/11/8
+ */
+public class BSTMap<K, V> implements Map<K, V> {
+
+    @Override
+    public void add(K key, V value) {
+
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void set(K key, V value) {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+    
+}
--- sandbox/src/main/java/com/cipher/data_structure/F_Set_And_Map/map/Main.java
+++ sandbox/src/main/java/com/cipher/data_structure/F_Set_And_Map/map/Main.java
@@ -1,5 +1,11 @@
 package com.cipher.data_structure.F_Set_And_Map.map;
 
+import com.cipher.data_structure.F_Set_And_Map.FileOperation;
+
+import java.util.ArrayList;
+
+import static java.lang.System.nanoTime;
+
 /**
  * @Author: CipherCui
  * @Description:
@@ -7,8 +13,31 @@ package com.cipher.data_structure.F_Set_And_Map.map;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String PATH = "src/main/java/com/cipher/data_structure/F_Set_And_Map/resource/";
+    private static final String A_TALE_OF_TWO_CITIES = PATH + "a-tale-of-two-cities.txt";
+    private static final String PRIDE_AND_PREJUDICE = PATH + "pride-and-prejudice.txt";
 
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        FileOperation.readFile(PRIDE_AND_PREJUDICE, words);
+        System.out.println("Total words: " + words.size());
+        System.out.println("----------------------------");
+
+        testSet(new LinkedListMap<>(), words);
+    }
+
+    private static void testSet(Map<String, Integer> map, ArrayList<String> words) {
+        long start = nanoTime();
+        for (String w : words) {
+            if (map.contains(w)) {
+                map.set(w, map.get(w) + 1);
+            } else {
+                map.add(w, 1);
+            }
+        }
+        long end = nanoTime();
+        System.out.println("Frequency of PRIDE: " + map.get("pride"));
+        System.out.println("Total different words: " + map.getSize() + " during " + (end - start) / 1000000000.0);
     }
 
 }
