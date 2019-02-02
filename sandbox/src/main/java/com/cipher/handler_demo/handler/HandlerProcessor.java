5632e733189a97cde9e8b26610a30e39989cd8bf
ciphermagic <ciphermagic@yeah.net>
2019-02-02 16:25:10 +0800

    reset

--- sandbox/src/main/java/com/cipher/handler_demo/handler/HandlerProcessor.java
+++ sandbox/src/main/java/com/cipher/handler_demo/handler/HandlerProcessor.java
@@ -1,6 +1,5 @@
 package com.cipher.handler_demo.handler;
 
-import com.cipher.handler_demo.util.BeanTool;
 import com.cipher.handler_demo.util.ClassScaner;
 import com.google.common.collect.Maps;
 import org.springframework.beans.BeansException;
@@ -37,25 +36,6 @@ public class HandlerProcessor implements BeanFactoryPostProcessor {
         });
         HandlerContext context = new HandlerContext(handlerMap);
         beanFactory.registerSingleton(HandlerContext.class.getName(), context);
-    }
-
-    /**
-     * 处理器上下文，根据类型获取相应的处理器
-     */
-    public static class HandlerContext {
-        private Map<String, Class> handlerMap;
-
-        public HandlerContext(Map<String, Class> handlerMap) {
-            this.handlerMap = handlerMap;
-        }
-
-        public AbstractHandler getInstance(String type) {
-            Class clazz = handlerMap.get(type);
-            if (clazz == null) {
-                throw new IllegalArgumentException("not found handler for type: " + type);
-            }
-            return (AbstractHandler) BeanTool.getBean(clazz);
-        }
     }
 
 }
