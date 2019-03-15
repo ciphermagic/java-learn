98946787578be0f94431fded39aff77072ba71ad
ciphermagic <ciphermagic@yeah.net>
2019-03-15 14:34:57 +0800

    netty

---
+++ sandbox/src/main/java/com/cipher/netty/NettyServer.java
@@ -0,0 +1,42 @@
+package com.cipher.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @Author: CipherCui
+ * @Description:
+ * @Date: Created in 14:13 2019/3/15
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                    }
+                });
+        bind(bootstrap, 1000);
+    }
+
+    private static void bind(final ServerBootstrap bootstrap, final int port) {
+        bootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.out.println("端口[" + port + "]绑定失败!");
+                bind(bootstrap, port + 1);
+            }
+        });
+    }
+
+}
