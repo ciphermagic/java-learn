7d70f527d955780a512854cd481048a8db145b09
ciphermagic <ciphermagic@yeah.net>
2019-05-30 23:56:22 +0800

    lambda to string

---
+++ sandbox/src/main/java/com/cipher/test/FnConverter.java
@@ -0,0 +1,35 @@
+package com.cipher.test;
+
+import java.beans.Introspector;
+import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
+
+public class FnConverter {
+
+    @FunctionalInterface
+    public interface Fn<T> extends Serializable {
+        Object apply(T source);
+    }
+
+    public static <T> String convertFnToString(Fn<T> fn) {
+        return fnToFieldName(fn);
+    }
+
+    public static String fnToFieldName(Fn fn) {
+        try {
+            Method method = fn.getClass().getDeclaredMethod("writeReplace");
+            method.setAccessible(Boolean.TRUE);
+            SerializedLambda serializedLambda = (SerializedLambda) method.invoke(fn);
+            return serializedLambda.getImplMethodName();
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String fieldName = FnConverter.convertFnToString(Foo::getBar);
+        System.out.println("方法名：" + fieldName);
+    }
+
+}
---
+++ sandbox/src/main/java/com/cipher/test/Foo.java
@@ -0,0 +1,15 @@
+package com.cipher.test;
+
+public class Foo {
+
+    private Integer bar;
+
+    public Integer getBar() {
+        return bar;
+    }
+
+    public void setBar(Integer bar) {
+        this.bar = bar;
+    }
+
+}
