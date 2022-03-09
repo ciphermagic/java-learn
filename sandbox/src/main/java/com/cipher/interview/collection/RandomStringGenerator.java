4d956101df43f0a366c914801f33536ce4c1db76
ciphermagic <ciphermagic@yeah.net>
2022-03-09 16:19:11 +0800

    add some file

---
+++ sandbox/src/main/java/com/cipher/interview/collection/RandomStringGenerator.java
@@ -0,0 +1,16 @@
+package com.cipher.interview.collection;
+
+import java.util.Iterator;
+
+/**
+ * @author cipher
+ */
+public class RandomStringGenerator<T> implements Iterable<T> {
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+}
--- sandbox/src/main/java/com/cipher/test/Solution.java
+++ sandbox/src/main/java/com/cipher/test/Solution.java
@@ -1,34 +1,76 @@
 package com.cipher.test;
 
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 class Solution {
+    Map<Integer, Integer> areaMap = new HashMap<>();
+    int[][] dirs = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-    public String minWindow(String s, String t) {
-        int m = s.length();
-        int n = t.length();
-        int[] hash = new int[128];
-        for (char c : t.toCharArray()) {
-            hash[c]--;
+    private boolean inArea(int[][] grid, int i, int j) {
+        return i >= 0 && i < grid.length && j >= 0 && j < grid[0].length;
+    }
+
+    public int largestIsland(int[][] grid) {
+        int res = 0;
+        int index = 2;
+        boolean hasIsland = false;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    areaMap.put(index, curArea(grid, i, j, index));
+                    index++;
+                    hasIsland = true;
+                }
+            }
         }
-        String res = "";
-        for (int i = 0, j = 0, cnt = 0; i < m; i++) {
-            hash[s.charAt(i)]++;
-            if (hash[s.charAt(i)] <= 0) {
-                cnt++;
+        if (!hasIsland) {
+            return 1;
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    res = Math.max(res, newArea(grid, i, j));
+                }
             }
-            while (cnt == n && hash[s.charAt(j)] > 0) {
-                hash[s.charAt(j++)]--;
-            }
-            if (cnt == n && ("".equals(res) || res.length() > i - j + 1)) {
-                res = s.substring(j, i + 1);
+        }
+        return res;
+    }
+
+    private int curArea(int[][] grid, int i, int j, int index) {
+        if (!inArea(grid, i, j)) {
+            return 0;
+        }
+        if (grid[i][j] != 1) {
+            return 0;
+        }
+        grid[i][j] = index;
+        int res = 1;
+        for (int[] dir : dirs) {
+            res += curArea(grid, i + dir[0], j + dir[1], index);
+        }
+        return res;
+    }
+
+    private int newArea(int[][] grid, int i, int j) {
+        Set<Integer> set = new HashSet<>();
+        int res = 1;
+        for (int[] dir : dirs) {
+            int ti = i + dir[0];
+            int tj = j + dir[1];
+            if (inArea(grid, ti, tj) && grid[ti][tj] != 0 && !set.contains(grid[ti][tj])) {
+                res += areaMap.get(grid[ti][tj]);
+                set.add(grid[ti][tj]);
             }
         }
         return res;
     }
 
     public static void main(String[] args) {
-        String res = new Solution().minWindow("ADOBECODEBANC", "ABC");
+        Solution solution = new Solution();
+        int i = solution.largestIsland(new int[][]{{1, 1}, {1, 1}});
         System.out.println();
     }
 }
